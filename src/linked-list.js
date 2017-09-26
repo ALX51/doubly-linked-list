d8d967e085e22b8254ee070905ef31e45a4c0db7
ALX51 <31862401+ALX51@users.noreply.github.com>
2017-09-26 19:32:39 +0300

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,74 @@
 const Node = require('./node');
+//не понял условие продерки метода append ( _head, _tail)
 
-class LinkedList {
-    constructor() {}
+class LinkedList extends Node {
+    constructor() {
+        super()
+        this.arr = [];
+        this.length = this.arr.length;        
+    }
 
-    append(data) {}
+    append(data) {        
+        this.arr.push(data);
+        this.length = this.arr.length;
+        return this;
+       
+    }
 
-    head() {}
+    head() {        
+        if (this.arr[0]) {
+            return this.arr[0];
+        } else {
+            return null;
+        }
+    }
 
-    tail() {}
+    tail() {        
+        if (this.arr[this.arr.length - 1]) {
+            return this.arr[this.arr.length - 1];
+        } else {
+            return null;
+        }
+    }
 
-    at(index) {}
+    at(index) {         
+        return this.arr[index];
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+        this.arr[index] = data;
+        this.length = this.arr.length;
+         return this;
+    }
 
-    isEmpty() {}
+    isEmpty() {        
+        if (this.arr == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    clear() {}
+    clear() {
+        this.length = 0;
+        this.arr.length = 0;
+         return this;        
+    }
 
-    deleteAt(index) {}
+    deleteAt(index) {         
+        this.arr.splice(index, 1);
+        this.length = this.arr.length;
+         return this;       
+    }
 
-    reverse() {}
+    reverse() {         
+        Array.prototype.reverse.call(this.arr);        
+         return this;        
+    }
 
-    indexOf(data) {}
+    indexOf(data) {        
+        return this.arr.indexOf(data);        
+    }
 }
 
-module.exports = LinkedList;
+module.exports = LinkedList; 
